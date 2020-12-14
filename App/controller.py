8a26f66e06be85b1e593971fb060b25c8875996a
AndresVilla203 <69772391+AndresVilla203@users.noreply.github.com>
2020-12-13 23:50:05 -0500

    Update controller.py

--- App/controller.py
+++ App/controller.py
@@ -27,6 +27,8 @@
 import config as cf
 from App import model
 import csv
+import os
+import datetime
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
@@ -39,13 +41,47 @@ recae sobre el controlador.
 # ___________________________________________________
 #  Inicializacion del catalogo
 # ___________________________________________________
-
+def init():
+    Taxis = model.TaxiAnalyzer()
+    return Taxis
 
 # ___________________________________________________
 #  Funciones para la carga de datos y almacenamiento
 #  de datos en los modelos
 # ___________________________________________________
-
+def loadTrips(Taxis):
+    for filename in os.listdir(cf.data_dir):
+        if filename.endswith('.csv'):
+            print('Cargando archivo: ' + filename)
+            loadFile(Taxis, filename)
+    return Taxis
+def loadFile(Taxis, tripfile):
+    tripfile = cf.data_dir + tripfile
+    input_file = csv.DictReader(open(tripfile, encoding="utf-8"),
+                                delimiter=",")
+    for trip in input_file:
+        model.Agregar_Compañia(Taxis, trip)
+        model.NuevasFechas(Taxis, trip)
+        model.Agregar_viaje(Taxis, trip)
+    return Taxis
 # ___________________________________________________
 #  Funciones para consultas
 # ___________________________________________________
+
+def Cantidad_Taxis(Taxis):
+    return model.Cantidad_Taxis(Taxis)
+def Cantidad_compañias(Taxis):
+    return model.Cantidad_compañias(Taxis)
+def TOP_TAXI(Taxis, top):
+    return model.TOP_TAXI(Taxis, top)
+def PUNTOS_IND(Taxis, date, top):
+    fecha = datetime.datetime.strptime(date, '%Y-%m-%d')
+    return model.PUNTOS_IND(Taxis, fecha.date(), top)
+def topPuntosTaxiMultiple(Taxis, dateIni, dateFin, top):
+    fechaIni = datetime.datetime.strptime(dateIni, '%Y-%m-%d')
+    fechaFin = datetime.datetime.strptime(dateFin, '%Y-%m-%d')
+    return model.topPuntosTaxiMultiple(Taxis, fechaIni.date(), fechaFin.date(), top)
+def MENOR_CA(Taxis, initCA, destCA, horaIni, horaFin):
+    horaI = datetime.datetime.strptime(horaIni, "%H:%M")
+    horaF = datetime.datetime.strptime(horaFin, "%H:%M")
+    return model.MENOR_CA(Taxis, initCA, destCA, horaI.time(), horaF.time())
